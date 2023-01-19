8a0c6c2a58b9498442410c11d1e6d565045bcbfd
Afonso Filho <afonso.rf@gmail.com>
2023-01-19 15:31:06 -0300

    :lipstick: feat: Aturalização do estilo e Otimização de funções

--- js/main.js
+++ js/main.js
@@ -12,43 +12,39 @@ const failureType = document.querySelector('#tipo')
 const hostnameA = document.querySelector('#hostA')
 const hostnameB = document.querySelector('#hostB')
 const partner = document.querySelector('#partner')
-const response = document.querySelector('#response')
 
 ////--------------------Events------------------------------------
-failureType.addEventListener('focus', () => {
+failureType.addEventListener('focus', (e) => {
     $(function () {
         $("#tipo").autocomplete({
             source: Object.keys(listFailureTypes[0])
         })
     })
 })
-failureType.addEventListener('keyup', () => {
+failureType.addEventListener('blur', (event) => {
+    let item = event.target.value.trim().toUpperCase()
+    let spanError = document.querySelector(`#${event.target.id}~span`)
+    let spanText = ''
+    if (item.length > 0) {
+        event.target.classList.add('incluedText')
+    }
+    if (item.length == 0) {
+        spanText = 'Campo Obrigatório'
+    } else if (Object.keys(listFailureTypes[0]).indexOf(item.trim()) == -1) {
+        spanText = 'Tipo Falha Não Cadastrada'
+    }
 
-})
-failureType.addEventListener('blur', () => {
-    if (failureType.value.length > 0) {
-        failureType.classList.add('incluedText')
-    }
-    if (Object.keys(listFailureTypes[0]).indexOf(failureType.value) == -1) {
-        console.log('Tipo não encontrado')
-    }
-})
-//
-hostnameA.addEventListener('keyup', () => {
-    
-})
-hostnameA.addEventListener('blur', () => {
-    if (hostnameA.value.length > 0) {
-        hostnameA.classList.add('incluedText')
-    }
-})
-//
-hostnameB.addEventListener('keyup', () => {
-    
-})
-hostnameB.addEventListener('blur', () => {
-    if (hostnameB.value.length > 0) {
-        hostnameB.classList.add('incluedText')
+    event.target.setCustomValidity(spanText)
+    spanError.innerText = spanText
+
+    const hostLast = document.querySelector('.hostname~.hostname')
+    if (item.includes("FALHA")) {
+        hostnameB.disabled = true
+        hostnameB.value = ''
+        hostLast.style.display = 'none'
+    } else {
+        hostnameB.disabled = false
+        hostLast.style.display = ''
     }
 })
 //
@@ -59,17 +55,24 @@ partner.addEventListener('focus', () => {
         })
     })
 })
-partner.addEventListener('keyup', () => {
-
-})
-partner.addEventListener('blur', () => {
-    if (partner.value.length > 0) {
-        partner.classList.add('incluedText')
+partner.addEventListener('blur', (event) => {
+    let item = event.target.value.trim().toUpperCase()
+    let spanError = document.querySelector(`#${event.target.id}~span`)
+    let spanText = ''
+    if (item.length > 0) {
+        event.target.classList.add('incluedText')
     }
-    if (listPartners.indexOf(partner.value) == -1) {
-        console.log('Parceiro não encontrado')
+    if (item.length == 0) {
+        spanText = 'Campo Obrigatório'
+    } else if (listPartners.indexOf(item.trim()) == -1) {
+        spanText = 'Parceiro Não Cadastrado'
     }
+    event.target.setCustomValidity(spanText)
+    spanError.innerText = spanText
 })
+//
+inputHostnameBlur(hostnameA, separador[0])
+inputHostnameBlur(hostnameB, separador[0])
 ////-----------------------------------------------------
 
 ////-------------------Functions---------------------------
@@ -83,35 +86,126 @@ function listJson(URL) {
     })
     return list
 }
+//
+function inputHostnameBlur(ELEMENT, SPLIT) {
+    ELEMENT.addEventListener('blur', () => {
+        let item = ELEMENT.value.trim().toUpperCase()
+        let spanError = document.querySelector(`#${ELEMENT.id}~span`)
+        let spanText = ''
 
-function gerar() {
-    let failureType = document.querySelector('#tipo').value
-    let hostA = document.getElementById('hostA').value
-    let hostB = document.getElementById('hostB').value
-    let selectPartner = document.querySelector('#partner').value
-    
-    let res = document.getElementById('response')
+        if (item.length == 0) {
+            spanText = 'Campo Obrigatório'
+        } else if (item.split(SPLIT).length < 4) {
+            spanText = `Hostmane Invalido!
+            Ex: UF${SPLIT}CNL${SPLIT}POP${SPLIT}XXX00
+            ou BR${SPLIT}UF${SPLIT}CNL${SPLIT}POP${SPLIT}XX${SPLIT}00`
+        }
 
-    let hostnames = [ hostA.split(separador[0]), hostB.split(separador[0])]
+        spanError.innerText = spanText
+        ELEMENT.setCustomValidity(spanText)
 
-    let pops = []
+        if (item.length > 0) {
+            ELEMENT.classList.add('incluedText')
+        }
+    })
+}
+//
+function popSearchCNL(HOST, JSON, SPLIT) {
+    let host = HOST.split(SPLIT)
+    let pop = {}
 
-    for (h of hostnames) {
-        for (i of listCnl) {
-            if (i.SIGLA == (h[1].length > 2 ? h[1]: h[2]).toUpperCase()) {
-                i.POP = (h[1].length > 2 ? h[2] : h[3]).toUpperCase()
-                pops.push(i)
+    if (host.length > 2) {
+        pop.POP = host[1].length > 2 ? host[2] : host[3]
+        for (let i of JSON) {
+            try {
+                if (i.SIGLA === (host[1].length > 2 ? host[1] : host[2])) {
+                    pop.UF = i.UF
+                    pop.SIGLA = i.SIGLA
+                    pop.MUNICIPIO = i.MUNICIPIO
+                }
+                console.log(erro)
+            } catch (erro) {
             }
         }
     }
+    return pop
+}
+//
+function tableSites(OBJ) {
+    try {
+        return `<tr>
+        <td>UF</td>
+        <td>${OBJ.UF}</td>
+    </tr>
+    <tr>
+        <td>MUNICIPIO</td>
+        <td>${OBJ.MUNICIPIO}</td>
+    </tr>
+    <tr>
+        <td>SIGLA</td>
+        <td>${OBJ.SIGLA}</td>
+    </tr>
+    <tr>
+        <td>POP</td>
+        <td>${OBJ.POP}</td>
+    </tr>
+    `
+    } catch { }
+}
+//
+function gerar() {
+    const failure = failureType.value.trim().toUpperCase()
+    const hostA = hostnameA.value.trim().toUpperCase()
+    const hostB = hostnameB.value.trim().toUpperCase()
+    const selectPartner = partner.value.trim().toUpperCase()
+    const response = document.getElementById('response')
+    const sites = document.querySelector('.sites')
 
-    pops.sort((a, b)=>{
+    let pops = [popSearchCNL(hostA, listCnl, separador[0]), popSearchCNL(hostB, listCnl, separador[0])]
+    pops.sort((a, b) => {
         return a.MUNICIPIO + a.POP < b.MUNICIPIO + b.POP ? -1 : a.MUNICIPIO + a.POP > b.MUNICIPIO + b.POP ? 1 : 0
     })
-
-    res.innerText = `${pops[0].UF}${separador[3]}${listFailureTypes[0][failureType]}${separador[3]}`+
-    `${pops[0].MUNICIPIO}${separador[2]}${pops[0].POP}${separador[1]}`+
-    `${pops[1].MUNICIPIO}${separador[2]}${pops[1].POP}${separador[3]}${selectPartner}`
+    if (failure.includes('FALHA')) {
+        if (failure.includes('ENERGIA')) {
+            response.innerText = `${pops[0].UF}${separador[3]}` +
+                `${listFailureTypes[0][failure]}${separador[3]}` +
+                `${pops[0].MUNICIPIO}${separador[2]}${pops[0].POP}${separador[1]}` +
+                `${pops[0].UF}${separador[0]}${pops[0].SIGLA}${separador[0]}${pops[0].POP}${separador[3]}` +
+                `${selectPartner}`
+        } else {
+            response.innerText = `${pops[0].UF}${separador[3]}` +
+                `${listFailureTypes[0][failure]}${separador[3]}` +
+                `${pops[0].MUNICIPIO}${separador[2]}${pops[0].POP}${separador[1]}` +
+                `${hostA}${separador[3]}` +
+                `${selectPartner}`
+        }
+    } else {
+        response.innerText = `${pops[0].UF}${separador[3]}` +
+            `${listFailureTypes[0][failure]}${separador[3]}` +
+            `${pops[0].MUNICIPIO}${separador[2]}${pops[0].POP}${separador[1]}` +
+            `${pops[1].MUNICIPIO}${separador[2]}${pops[1].POP}${separador[3]}` +
+            `${selectPartner}`
+    }
+    sites.innerHTML = ''
+    for (i in pops) {
+        if (pops[i].UF) {
+            if (i == 0) {
+                sites.innerHTML += `<table>
+                <tr>
+                    <th colspan="2">PONTA A</th>
+                </tr>
+                ${tableSites(pops[i])}
+                </table>`
+            } else {
+                sites.innerHTML += `<table>
+                <tr>
+                    <th colspan="2">PONTA B</th>
+                </tr>
+                ${tableSites(pops[i])}
+                </table>`
+            }
+        }
+    }
 }
 
 ////---------------------------------------------------
@@ -124,6 +218,8 @@ for (let field of fields) {
     field.addEventListener("invalid", event => {
         event.preventDefault()
         console.log(event)
+        document.getElementById('response').innerText = ''
+        document.querySelector('.sites').innerHTML = ''
     })
 }
 
